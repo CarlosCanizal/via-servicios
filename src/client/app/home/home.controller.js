b850e93e1bdf6821f0228db1110e6285bb540329
mr-code-maker <carlos@codecery.com>
2017-03-08 22:06:17 -0600

    Creates carrousel

--- src/client/app/home/home.controller.js
+++ src/client/app/home/home.controller.js
@@ -11,5 +11,27 @@
     var home =  this;
     home.section = 'main';
 
+    var carouselWidth = $('.carousel-container').width();
+    var items =  $('nav.item').length;
+    home.items =  items;
+
+    home.selected = 0;
+
+    var totalWidth =  items*carouselWidth;
+    $('.carousel-riel').width(totalWidth);
+    $('nav.item').width(carouselWidth);
+
+    home.getNumber = function(num) {
+      return new Array(num);   
+    }
+
+    home.selectIt = function(index){
+      home.selected = index;
+      var left = index*carouselWidth*-1;
+      $('.carousel-riel').animate({left: left}, 300);
+    }
+
+
   }
+
 })();
